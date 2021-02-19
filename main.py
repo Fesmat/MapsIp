53aaf1996879bde138b7bde8a9ec99d29a684eba
Fesmat <dden4929@gmail.com>
2021-02-19 23:01:32 +0300

    Third task edited.

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def write_image():
 if __name__ == '__main__':
     coords = input("введите координаты объекта==>")
     zoom = int(input("введите масштаб карты==>"))
+    left, right = None, None
     pygame.init()
     size = WIDTH, HEIGHT = 600, 450
     screen = pygame.display.set_mode(size)
@@ -29,6 +30,7 @@ if __name__ == '__main__':
                 os.remove('map.png')
                 pygame.quit()
                 sys.exit()
+            print(coords)
             if i.type == pygame.KEYDOWN and i.key == pygame.K_PAGEUP:
                 if zoom < 13:
                     zoom += 1
@@ -39,30 +41,30 @@ if __name__ == '__main__':
                 write_image()
             if i.type == pygame.KEYDOWN and i.key == pygame.K_LEFT:
                 coords = list(map(float, coords.split(',')))
-                coords[0] -= zoom * 0.5
+                coords[0] -= 10 / zoom
                 if coords[0] < -180:
-                    coords[0] += zoom * 0.5
+                    coords[0] += 10 / zoom
                 coords = ','.join(map(str, coords))
                 write_image()
             if i.type == pygame.KEYDOWN and i.key == pygame.K_RIGHT:
                 coords = list(map(float, coords.split(',')))
-                coords[0] += zoom * 0.5
+                coords[0] += 10 / zoom
                 if coords[0] > 180:
-                    coords[0] -= zoom * 0.5
+                    coords[0] -= 10 / zoom
                 coords = ','.join(map(str, coords))
                 write_image()
             if i.type == pygame.KEYDOWN and i.key == pygame.K_UP:
                 coords = list(map(float, coords.split(',')))
-                coords[1] += zoom * 0.5
+                coords[1] += 10 / zoom
                 if coords[1] > 90:
-                    coords[0] -= zoom * 0.5
+                    coords[0] -= 10 / zoom
                 coords = ','.join(map(str, coords))
                 write_image()
             if i.type == pygame.KEYDOWN and i.key == pygame.K_DOWN:
                 coords = list(map(float, coords.split(',')))
-                coords[1] -= zoom * 0.5
+                coords[1] -= 10 / zoom
                 if coords[1] < -90:
-                    coords[1] += zoom * 0.5
+                    coords[1] += 10 / zoom
                 coords = ','.join(map(str, coords))
                 write_image()
         clock.tick(50)
